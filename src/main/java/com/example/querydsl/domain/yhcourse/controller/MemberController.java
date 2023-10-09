abce6e02b500b9b50843d658510342c86529e6c5
kwongwangjae <kgj9906@naver.com>
2023-10-10 01:50:40 +0900

    feat
    feat: 작업 yml파일 환경 분리 및 자동 생성 환경 추가

---
+++ src/main/java/com/example/querydsl/domain/yhcourse/controller/MemberController.java
@@ -0,0 +1,23 @@
+package com.example.querydsl.domain.yhcourse.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.querydsl.domain.yhcourse.dto.MemberSearchCondition;
+import com.example.querydsl.domain.yhcourse.dto.MemberTeamDto;
+import com.example.querydsl.domain.yhcourse.repository.MemberJpaRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+	private final MemberJpaRepository memberJpaRepository;
+	// @GetMapping("/v1/members")
+	// public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition)
+	// {
+	// 	return
+	// }
+}
